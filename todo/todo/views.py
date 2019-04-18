3b2a2ee8c1fb3aa4fbf99e8c6590145c43ecb4c0
Beikut00 <Beikut00>
2019-04-18 20:28:35 +0600

    first commit

--- todo/todo/views.py
+++
@@ -1,9 +0,0 @@
-
-from django.shortcuts import render
-from .models import *
-
-
-def index(request):
-	todolist = TodoList.objects.all()
-	context = {'todolist' : todolist}
-	return render(request, 'todo/index.html', context)
